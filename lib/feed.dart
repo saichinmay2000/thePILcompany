168b0543e4c0a7c460b86067e2af9da358186b59
sumanthnani10 <sumanthnani10@gmail.com>
2021-05-25 17:57:11 +0530

    feed edited

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class feed extends StatelessWidget {
   const feed({Key key}) : super(key: key);
@@ -8,6 +10,7 @@ class feed extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Container(
               width: MediaQuery.of(context).size.width,
@@ -16,12 +19,12 @@ class feed extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(20,0,0,0),
+                    padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
                     child: Icon(Icons.menu),
                   ),
                   Image.asset("assets/logo_white.png"),
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(0,0,20,0),
+                    padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
                     child: Icon(Icons.search),
                   ),
                 ],
@@ -29,16 +32,82 @@ class feed extends StatelessWidget {
               height: 75,
               alignment: Alignment.centerRight,
             ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              color: Colors.black,
-              height: 400,
-              child: Row(
-                children: <Widget>[
-                  Image.asset(
-                      "assets/image.jpg",
-                      height: 500,
-                      width: MediaQuery.of(context).size.width,
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(8, 0, 8, 16),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              CircleAvatar(
+                                radius: 20,
+                                foregroundImage: AssetImage("assets/logo.jpg"),
+                              ),
+                              SizedBox(
+                                width: 8,
+                              ),
+                              Text("PIL Admin",
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white)),
+                              Flexible(
+                                child: Container(),
+                              )
+                            ],
+                          ),
+                        ),
+                        Image.asset(
+                          "assets/image.jpg",
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
+                          child: Column(
+                            children: [
+                              Text(
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+                                maxLines: 3,
+                                style: TextStyle(color: Colors.white60),
+                                textAlign: TextAlign.left,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  IconButton(
+                                      icon: Icon(
+                                        CupertinoIcons.heart_fill,
+                                      ),
+                                      onPressed: () async {}),
+                                  IconButton(
+                                      icon: Icon(Icons.share),
+                                      onPressed: () async {}),
+                                  IconButton(
+                                      icon: Icon(Icons.bookmark_border),
+                                      onPressed: () async {}),
+                                  Flexible(
+                                    child: Container(),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
